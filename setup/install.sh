a1d2f6ce5af407993e1bf93b80a153fb9d8582ed
kedde <christianthillemann@gmail.com>
2015-04-14 22:30:13 +0200

    fixed wrong folder structure

--- setup/install.sh
+++ setup/install.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
-cd ..
 
 if [[ -e $HOME/.vimrc || -e $HOME/.vim ]]; then
   echo "Ooops: ~/.vimrc or ~/.vim already exists. \n\
        It looks like you already have some vim configuration. \n\
        Try to run ./uninstall.sh"
 else
+  cd ..
   echo === Checking out vundle
   git submodule update --init
   cd -
+
+  echo "after vundle"
+  cd ..
 
   echo === Linking vim directories
   ln -s $PWD/vimrc $HOME/.vimrc
